1b6b67738b3ceda4dec024b153d57b568445b923
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2023-01-10 09:50:28 +0100

    basic structure created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css"
+import Welcome from "./routes/Welcome";
+import Adventure from "./routes/Adventure";
+import { Route, Routes } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/adventure" element={<Adventure />} />
+    </Routes>
+    </>
   );
 }
 
